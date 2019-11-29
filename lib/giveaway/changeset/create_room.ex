6309d0b81018e43fe0f25d3cfaa4273e0f3104b6
Nick Dichev <nickdichev@yahoo.com>
2019-11-29 14:10:25 -0800

    allow underscore in room name

--- lib/giveaway/changeset/create_room.ex
+++ lib/giveaway/changeset/create_room.ex
@@ -1,7 +1,7 @@
 defmodule Giveaway.Changeset.CreateRoom do
   defstruct [:room_name]
 
-  @format_error "Room name may only contain alphanumeric characters."
+  @format_error "Room name may only contain alphanumeric characters and underscore."
 
   def changeset(room, params \\ %{}) do
     {types, permitted} = types()
@@ -9,7 +9,7 @@ defmodule Giveaway.Changeset.CreateRoom do
     {room, types}
     |> Ecto.Changeset.cast(params, permitted)
     |> Ecto.Changeset.validate_required(:room_name)
-    |> Ecto.Changeset.validate_format(:room_name, ~r/^[a-zA-Z0-9]+$/, message: @format_error)
+    |> Ecto.Changeset.validate_format(:room_name, ~r/^[a-zA-Z0-9_]+$/, message: @format_error)
   end
 
   defp types() do

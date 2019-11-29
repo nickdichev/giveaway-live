420876186ef0f2280e88cffe7120cc8c9212a48b
Nick Dichev <nickdichev@yahoo.com>
2019-11-29 14:00:11 -0800

    Only allow room names to be one word, only alphanumeric

--- lib/giveaway/changeset/create_room.ex
+++ lib/giveaway/changeset/create_room.ex
@@ -1,12 +1,15 @@
 defmodule Giveaway.Changeset.CreateRoom do
   defstruct [:room_name]
 
+  @format_error "Room name may only contain alphanumeric characters."
+
   def changeset(room, params \\ %{}) do
     {types, permitted} = types()
 
     {room, types}
     |> Ecto.Changeset.cast(params, permitted)
     |> Ecto.Changeset.validate_required(:room_name)
+    |> Ecto.Changeset.validate_format(:room_name, ~r/^[a-zA-Z0-9]+$/, message: @format_error)
   end
 
   defp types() do

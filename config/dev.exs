e937ec275de2fb74dbe2e98e0120503db7f03018
Nick Dichev <nick@dichev.email>
2020-01-11 19:23:32 -0800

    Set room timeout to infinity

--- config/dev.exs
+++ config/dev.exs
@@ -66,4 +66,4 @@ config :phoenix, :stacktrace_depth, 20
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
 
-config :giveaway, :room_timeout, 20_000
+config :giveaway, :room_timeout, :infinity
